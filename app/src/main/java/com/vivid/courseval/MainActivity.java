b2213aaa89657feacf68fec257a6fdbaecf164ec
NattieVivid <nattie.vivid@gmail.com>
2016-02-28 14:09:10 +0200

    Commit

--- app/src/main/java/com/vivid/courseval/MainActivity.java
+++ app/src/main/java/com/vivid/courseval/MainActivity.java
@@ -52,6 +52,8 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //--
+
        /* Toast toast = Toast.makeText(getApplicationContext(), Locale.getDefault().getLanguage(),
                 Toast.LENGTH_SHORT);
         toast.show();*/
